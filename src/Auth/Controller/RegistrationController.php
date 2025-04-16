9746ba62fc45e88463d9d08b7bf5e9a6e3635cd1
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-04-16 22:45:31 +0200

    update type account on cart

--- src/Auth/Controller/RegistrationController.php
+++ src/Auth/Controller/RegistrationController.php
@@ -16,6 +16,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class RegistrationController extends AbstractController
 {
     #[Route('/register/customer', name: 'register_customer')]
+    #[IsGranted('ROLE_CUSTOMER')]
+
     public function registerCustomer(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -46,6 +48,7 @@ class RegistrationController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+        $user->setRoles(['ROLE_ADMIN']);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $plainPassword = $form->get('plainPassword')->getData();
@@ -53,7 +56,6 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-        $user->setRoles(['ROLE_ADMIN']);
 
             return $security->login($user, AppCustomAuthenticator::class, 'main');
         }
